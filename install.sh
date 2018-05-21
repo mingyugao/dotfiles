f91ea69749371681e24892f07ed4c80cd76730e4
Mingyu Gao <mingyu.gao96@gmail.com>
2018-05-21 00:34:08 -0700

    fix typo

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@ brew install the_silver_searcher
 # Create symlinks for dotfiles
 ln -s ~/.dotfiles/bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/bashrc ~/.bashrc
-ln -s ~/.dotfiles/tmux.conf ~/.gitconfig
+ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/vimrc ~/.vimrc

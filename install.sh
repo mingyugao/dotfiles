6a5f6fbff6e724ebbc13e8ff32b35a678a70b8c3
Mingyu Gao <mingyu.gao96@gmail.com>
2018-08-29 20:47:54 -0700

    force symlink creation on .zshrc

--- install.sh
+++ install.sh
@@ -22,4 +22,4 @@ ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/vimrc ~/.vimrc
-ln -s ~/.dotfiles/zshrc ~/.zshrc
+ln -fs ~/.dotfiles/zshrc ~/.zshrc
